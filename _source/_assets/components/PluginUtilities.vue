d5fd8c0446026e93091a11327e67b9c7932e41b1
Will Browar <wbrowar@gmail.com>
2021-09-01 12:16:09 -0400

    Added Guide export options

--- _source/_assets/components/PluginUtilities.vue
+++ _source/_assets/components/PluginUtilities.vue
@@ -7,10 +7,21 @@
       <em>NOTE: Twig templates and assets will need to be migrated manually.</em>
     </p>
     <hr />
-    <p><b>Step 1.</b> Copy Guide data by clicking this button.</p>
+    <p><b>Step 1.</b> Select Guides to export.</p>
+    <div v-for="option in exportGuideOptions" :key="option.slug">
+      <input
+        :id="`export-option-${option.slug}`"
+        v-model="option.checked"
+        class="checkbox"
+        type="checkbox"
+        :name="option.slug"
+      />
+      <label :for="`export-option-${option.slug}`">{{ option.title }}</label>
+    </div>
+    <p><b>Step 2.</b> Copy Guide data by clicking this button.</p>
     <button class="btn submit" type="button" @click="copyExportData">Copy Guide Data</button>
-    <p><b>Step 2.</b> Log into another environment and visit this <b>Utilities → Guide</b> page</p>
-    <p><b>Step 3.</b> In that environment, paste the JSON into this field and click "Import"</p>
+    <p><b>Step 3.</b> Log into another environment and visit this <b>Utilities → Guide</b> page</p>
+    <p><b>Step 4.</b> In that environment, paste the JSON into this field and click "Import"</p>
     <CraftFieldText
       ref="importField"
       :field-attributes="{ rows: 4 }"
@@ -27,6 +38,7 @@
 import { defineComponent, reactive, toRefs } from 'vue';
 import { log } from '../globals';
 import CraftFieldText from './CraftFieldText.vue';
+import { Guide } from '~/types/plugins';
 
 export default defineComponent({
   name: 'PluginUtilities',
@@ -36,15 +48,28 @@ export default defineComponent({
   props: {
     exportData: String,
   },
-  setup: () => {
+  setup: (props) => {
     const state = reactive({
       dataImported: false,
+      exportGuides: JSON.parse(props.exportData),
       importData: '',
     });
 
     return { ...toRefs(state) };
   },
-  computed: {},
+  computed: {
+    exportGuideOptions() {
+      return this.exportGuides?.guides?.length
+        ? this.exportGuides.guides.map((guide) => {
+            return {
+              checked: false,
+              slug: guide.slug,
+              title: guide.title,
+            };
+          })
+        : [];
+    },
+  },
   methods: {
     copyText(text, notification) {
       navigator.clipboard.writeText(text).then(() => {
@@ -52,8 +77,27 @@ export default defineComponent({
       });
     },
     copyExportData() {
-      if (this.exportData) {
-        this.copyText(this.exportData, 'Data copied');
+      const selectedData: string[] = [];
+      this.exportGuideOptions.forEach((option: { checked: boolean; slug: string; title: string }) => {
+        if (option.checked) {
+          selectedData.push(option.slug);
+        }
+      });
+
+      const filteredGuides = this.exportGuides?.guides?.length
+        ? this.exportGuides.guides.filter((guide: Guide) => {
+            return selectedData.includes(guide.slug);
+          })
+        : [];
+
+      const data = {
+        guides: filteredGuides,
+      };
+
+      const exportDataString = JSON.stringify(data);
+
+      if (exportDataString) {
+        this.copyText(exportDataString, 'Data copied');
       }
     },
     onImportDataChanged(newValue) {
